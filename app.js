83b5ae7262e1fad1095e0447fc13ad5bda80dcc6
Angela Mitchell <angela.mitchell@vmlyr.com>
2021-06-22 10:39:04 -0500

    toggle visibility dynamically

--- app.js
+++ app.js
@@ -3,20 +3,40 @@ const app = Vue.createApp({
         return {
             userInput: '',
             user1Entered: false,
-            user2Entered: false
+            user2Entered: false,
+            isVisible: true,
         }
     },
     methods: {
-        addStyle() {
+        userInputEntered() {
             if (this.userInput.toLowerCase() === 'user1') {
                 this.user1Entered = true;
                 this.user2Entered = false;
             } else if (this.userInput.toLowerCase() === 'user2') {
-                this.user2Entered = true;
                 this.user1Entered = false;
+                this.user2Entered = true;
+            }
+        },
+        toggleVisibility() {
+            this.isVisible = !this.isVisible;
+        }
+    },
+    computed: {
+        colors() {
+            if (this.userInput.toLowerCase() === 'user1') {
+                return { user1: this.user1Entered };
+            } else if (this.userInput.toLowerCase() === 'user2') {
+                return { user2: this.user2Entered };
+            }
+        },
+        visibility() {
+            if (this.isVisible) {
+                return { visible: this.isVisible };
+            } else {
+                return { hidden: !this.isVisible };
             }
         }
-    }
+    },
 });
 
 app.mount('#assignment');
