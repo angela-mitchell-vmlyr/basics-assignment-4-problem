560eb56db54a2f15311b21c4aea634479d3cf049
Angela Mitchell <angela.mitchell@vmlyr.com>
2021-06-22 11:49:39 -0500

    bind inputClass on first p tag to dynamically style it based on user input

--- app.js
+++ app.js
@@ -1,24 +1,13 @@
 const app = Vue.createApp({
     data() {
         return {
-            userInput1: '',
+            inputClass: '',
             userInput2: '',
-            user1Entered: false,
-            user2Entered: false,
             isVisible: true,
             colorEntered: false,
         }
     },
     methods: {
-        userInput1Entered() {
-            if (this.userInput1.toLowerCase() === 'user1') {
-                this.user1Entered = true;
-                this.user2Entered = false;
-            } else if (this.userInput1.toLowerCase() === 'user2') {
-                this.user1Entered = false;
-                this.user2Entered = true;
-            }
-        },
         toggleVisibility() {
             this.isVisible = !this.isVisible;
         },
@@ -29,13 +18,6 @@ const app = Vue.createApp({
         }
     },
     computed: {
-        colors() {
-            if (this.user1Entered) {
-                return { user1: this.user1Entered };
-            } else if (this.user2Entered) {
-                return { user2: this.user2Entered };
-            }
-        },
         visibility() {
             if (this.isVisible) {
                 return { visible: this.isVisible };
